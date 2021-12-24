e1b5e8720e63edd8d39ed2003367fd9f5d94a2a0
HugoUlm <hulmgren@gmail.com>
2021-12-24 02:40:35 +0100

    Initial Commit

---
+++ Rooster/RoosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoosterApp.swift
+//  Rooster
+//
+//  Created by Hugo Bäckman Ulmgren on 2021-12-24.
+//
+
+import SwiftUI
+
+@main
+struct RoosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
