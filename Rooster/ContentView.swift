da77aa88183d335649cca9f7d5b93f4137b6fb01
HugoUlm <hulmgren@gmail.com>
2021-12-24 23:56:38 +0100

    init commit

--- Rooster/ContentView.swift
+++ Rooster/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedIndex = 0
+    
+    let icons = [
+        "house",
+        "magnifyingglass",
+        "heart",
+        "cart",
+        "person"
+    ]
+    
+    let iconNames = [
+        "Home",
+        "Search",
+        "Favorites",
+        "Cart",
+        "Profile"
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            // Content
+            ZStack {
+                switch selectedIndex {
+                case 0:
+                    HomeView()
+                case 1:
+                    SearchView()
+                case 2:
+                    FavoriteView()
+                case 3:
+                    CartView()
+                default:
+                    UserView()
+                }
+            }
+
+            Divider()
+            
+            HStack {
+                ForEach(0..<5, id: \.self) {number in
+                    Spacer()
+                    Button(action: {
+                        self.selectedIndex = number
+                    }, label: {
+                        VStack {
+                            Image(systemName: icons[number])
+                                .font(.system(size: 25, weight: .regular, design: .default))
+                                .foregroundColor(selectedIndex == number ? Color(UIColor(red: 0.451, green: 0, blue: 0.6275, alpha: 1.0)) : Color(UIColor.lightGray))
+                            
+                            Text(iconNames[number])
+                                .font(.system(size: 10, weight: .regular, design: .default))
+                                .foregroundColor(selectedIndex == number ? Color(UIColor(red: 0.451, green: 0, blue: 0.6275, alpha: 1.0)) : Color(UIColor.lightGray))
+                        }
+                    })
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+        }
     }
 }
