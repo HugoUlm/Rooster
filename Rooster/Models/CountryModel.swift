76d42281e85dcf058c7a41c8a33ab2fea828edf9
HugoUlm <hulmgren@gmail.com>
2021-12-26 03:43:57 +0100

    model added

---
+++ Rooster/Models/CountryModel.swift
@@ -0,0 +1,19 @@
+//
+//  CountryModel.swift
+//  Rooster
+//
+//  Created by Hugo Bäckman Ulmgren on 2021-12-26.
+//
+
+import Foundation
+
+struct CountryModel {
+    // Fetch countries through API later
+    
+    
+    static let names = [
+        "Sweden",
+        "USA",
+        "Germany"
+    ]
+}
