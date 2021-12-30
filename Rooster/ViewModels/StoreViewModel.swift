b77e181252d15378165c0770f12d8d1aa90f273a
HugoUlm <hulmgren@gmail.com>
2021-12-30 23:48:19 +0100

    added fetch of products from firestore and image creator from firebase storage URL

--- Rooster/ViewModels/StoreViewModel.swift
+++ Rooster/ViewModels/StoreViewModel.swift
@@ -5,37 +5,67 @@
 //  Created by Hugo Bäckman Ulmgren on 2021-12-28.
 //
 
+import SwiftUI
 import Foundation
-import Firebase
 import FirebaseFirestore
+import FirebaseStorage
 
 class StoreViewModel: ObservableObject {
-    @Published var products = [ProductModel]()
     
-    func getCustomers() {
+    @Published var products = [ProductModel]()
+
+    func getProducts() {
         // Get reference
         let db = Firestore.firestore()
         
         // Read data
-        db.collection("customers").getDocuments {  snapshot, error in
+        db.collection("products").getDocuments {  snapshot, error in
             // Check for error
             if error == nil {
                 // No error
                 
                 if let snapshot = snapshot {
                     
-                    // Get all docs and create customer
-                    //self.products = snapshot.documents.map { doc in
-                        
-                        // Create new customer for each doc
-//                        return CustomerModel(id: doc.documentID, email: doc["email"] as? String ?? "", firstName: doc["firstName"] as? String ?? "", lastName: doc["lastName"] as? String ?? "", passwordHash: doc["passwordHash"] as? String ?? "", country: doc["country"] as? String ?? "", gender: doc["gender"] as? String ?? "", dateOfBirth: doc["dateOfBirth"] as? Date ?? Date.distantPast)
+                    DispatchQueue.main.async {
+                        // Get all docs and create customer
+                        self.products = snapshot.documents.map { doc in
+                            
+                            // Create new customer for each doc
+                            return ProductModel(id: doc.documentID, brand: doc["brand"] as? String ?? "", model: doc["model"] as? String ?? "", price: doc["price"] as? Int ?? 0, sizes: doc["sizes"] as? [Int] ?? [0], image: doc["image"] as? String ?? "No image")
+                        }
                     }
                 }
             }
-            //else {
+            else {
                 // Handle error
                 
-            //}
+            }
         }
     }
-//}
+    
+    @Published var dataIsValid = false
+    var data:Data?
+
+    // Fetches image from Firebase Storage via URL
+    func loadImage(url: String) {
+        // Set reference
+        let imageRef = Storage.storage().reference(forURL: url)
+        // Make sure file is < 1MB
+        imageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            // Catch error
+            if let error = error {
+                print("\(error)")
+            }
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                print(self.dataIsValid)
+                self.dataIsValid = true
+                self.data = data
+            }
+        }
+    }
+    // Create image from data in "loadImage" func
+    func imageFromData() -> UIImage {
+        UIImage(data: self.data!)!
+    }
+}
