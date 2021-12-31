35f290cc8fdb725cee8af235d30f99be4ab6998d
HugoUlm <hulmgren@gmail.com>
2021-12-31 04:11:41 +0100

    cards for products added

--- Rooster/Views/StoreView.swift
+++ Rooster/Views/StoreView.swift
@@ -6,57 +6,128 @@
 //
 
 import SwiftUI
-import WebKit
+import SDWebImageSwiftUI
 
 struct StoreView: View {
     
     @ObservedObject var viewModel = StoreViewModel()
     
     var body: some View {
-        
-        ForEach(viewModel.products, id: \.id) { product in
-            VStack {
+        Group {
+            VStack(alignment: .leading) {
                 Text("NIKE")
                     .font(.system(size: 20))
+                    .offset(x: 15, y: 0)
+                ScrollView(.horizontal) {
+                    nikeView(viewModel: viewModel)
+                }
+            }
+            .offset(x: 0, y: 0)
+        }
+        
+        Spacer()
+            .frame(height: 50)
+        
+        Group {
+            VStack(alignment: .leading) {
                 Text("PUMA")
                     .font(.system(size: 20))
-            }
-
-            HStack {
-                if product.brand == "Nike" {
-                    ZStack {
-                        Image(uiImage: viewModel.dataIsValid ? viewModel.imageFromData(): UIImage())
-                            .resizable()
-                            .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Text(product.model)
-                    }
-
-                    .onAppear {
-                        viewModel.loadImage(url: product.image)
-                    }
+                    .offset(x: 15, y: 0)
+                ScrollView(.horizontal) {
+                    pumaView(viewModel: viewModel)
                 }
             }
-            HStack {
-                if product.brand == "Puma" {
-                    ZStack {
-                        Image(uiImage: viewModel.dataIsValid ? viewModel.imageFromData(): UIImage())
-                            .resizable()
-                            .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Text(product.model)
-                    }
-                    
-                    .onAppear {
-                        viewModel.loadImage(url: product.image)
-                    }
-                }
-            }
+            .offset(x: 0, y: 0)
         }
-    }
 
+
+                
+        }
     init() {
         viewModel.getProducts()
     }
 }
+
+
+
+@ViewBuilder
+func nikeView(viewModel: StoreViewModel) -> some View {
+    HStack {
+        ForEach(viewModel.products, id: \.id) { product in
+            if product.brand == "Nike" {
+                VStack {
+
+                    AnimatedImage(url: URL(string: product.image))
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+
+                    HStack {
+                        VStack(alignment: .leading, content: {
+                            Text("$" + String(product.price))
+                                .font(.headline)
+                                .foregroundColor(.secondary)
+                            Text(product.model.uppercased())
+                                .font(.title2)
+                                .foregroundColor(.primary)
+                        })
+                        .layoutPriority(100)
+
+                        Spacer()
+                    }
+                    .padding()
+                }
+                .frame(width: 200, height: 250)
+                .cornerRadius(10)
+                .overlay (
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(UIColor.gray), lineWidth: 1)
+                )
+                    .padding([.top, .horizontal])
+            }
+
+        }
+    }
+}
+
+@ViewBuilder
+func pumaView(viewModel: StoreViewModel) -> some View {
+    HStack {
+        ForEach(viewModel.products, id: \.id) { product in
+            if product.brand == "Puma" {
+                VStack {
+
+                    AnimatedImage(url: URL(string: product.image))
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+
+                    HStack {
+                        VStack(alignment: .leading, content: {
+                            Text("$" + String(product.price))
+                                .font(.headline)
+                                .foregroundColor(.secondary)
+                            Text(product.model.uppercased())
+                                .font(.title2)
+                                .foregroundColor(.primary)
+                        })
+                        .layoutPriority(100)
+
+                        Spacer()
+                    }
+                    .padding()
+                }
+                .frame(width: 200, height: 250)
+                .cornerRadius(10)
+                .overlay (
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(UIColor.gray), lineWidth: 1)
+                )
+                    .padding([.top, .horizontal])
+            }
+
+        }
+    }
+}
+
 
 struct StoreView_Previews: PreviewProvider {
     static var previews: some View {
